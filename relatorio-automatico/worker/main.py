283fb72e454ed77256aaa5b6f019f6036345566f
RyronMarins <ryronmarins@gmail.com>
2023-10-06 10:02:10 -0300

    v1.1.0

--- relatorio-automatico/worker/main.py
+++ relatorio-automatico/worker/main.py
@@ -206,7 +206,7 @@ for idx, arquivo_tr_user in enumerate(arquivos_tr_user, start=1):
 
     # Copiar os CPFs filtrados para a planilha BASE.PF na planilha Matriz Estudo Mensal
     sheet_base_pf = workbook_matriz_estudo['BASE.PF']
-    for cpf in cpf_values_filtered[1:]:  # Começando a partir da segunda linha para evitar o cabeçalho
+    for cpf in cpf_values_filtered[0:]:  
         sheet_base_pf.append([cpf])
 
     # Salvar as mudanças no arquivo TR_USER atual
